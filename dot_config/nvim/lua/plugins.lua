66d7bbeed26f53d822217196cdb12ddadf7978e8
Ben Robertson <benrobertson150@hotmail.co.uk>
2025-11-01 22:58:15 +0000

    Add lsp

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -33,7 +33,7 @@ require("lazy").setup({
     config = function()
         local lspconfig = require("lspconfig")
 	lspconfig.lua_ls.setup({})
-        lspconfig.qmlls.setup({})
+        -- lspconfig.qmlls.setup({})
     end,
 },
     {
